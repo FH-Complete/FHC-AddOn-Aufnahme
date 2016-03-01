721394a5c94bf648b97815e1519ea3d0f73b9429
Paminger <christian@paminger.at>
2016-03-01 10:55:07 +0100

    PHPCI

---
+++ test/codeception/tests/acceptance/WelcomeCept.php
@@ -0,0 +1,14 @@
+<?php 
+// @group login
+
+$I = new AcceptanceTester($scenario);
+$I->am('Anonymous User'); 
+$I->wantTo('ensure that Home-Page works');
+
+$I->lookForwardTo('check the Home Page');
+$I->amOnPage('/');
+$I->see("window.location.href='index.php';");
+
+$I->lookForwardTo('check the Welcome Page');
+$I->amOnPage('/index.php');
+$I->see('Willkommen');
