7691c3bebfc5afa74c74fc50bf18ec6173bc8b0d
Paminger <christian@paminger.at>
2016-03-01 14:41:32 +0100

    index.php

--- cis/index.dist.php
+++ cis/index.dist.php
@@ -97,7 +97,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = '../../../vendor/codeigniter/framework/system';
+	$system_path = '/home/fhc/FHC-Core/vendor/codeigniter/framework/system';
 
 /*
  *---------------------------------------------------------------
--- test/codeception/tests/acceptance/WelcomeCept.php
+++ test/codeception/tests/acceptance/WelcomeCept.php
@@ -6,9 +6,9 @@ $I->am('Anonymous User');
 $I->wantTo('ensure that Home-Page works');
 
 $I->lookForwardTo('check the Home Page');
-$I->amOnPage('/index.html');
+$I->amOnPage('index.html');
 $I->see("window.location.href='index.php';");
 
 $I->lookForwardTo('check the Welcome Page');
-$I->amOnPage('/index.php');
+$I->amOnPage('index.php');
 $I->see('Willkommen');
