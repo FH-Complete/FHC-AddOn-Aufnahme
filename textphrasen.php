e98e1ace92473bc415cb5453aa93ac8e2875e81a
Nikolaus Krondraf <krondraf@technikum-wien.at>
2016-07-01 13:18:09 +0200

    Phrasen hinzugefügt

--- textphrasen.php
+++ textphrasen.php
@@ -7,7 +7,7 @@ $textphrasen = array
 		'phrasentext' => array
 		(
 			'sprache' => 'de-AT',
-			'text' => 'Herzlich Willkomen!',
+			'text' => 'Willkommen bei der Online Bewerbung',
 			'description' => 'Begrüßungstext am HomeScreen' 
 		)
 	),
@@ -17,8 +17,68 @@ $textphrasen = array
 		'phrasentext' => array
 		(
 			'sprache' => 'en-US',
-			'text' => 'Welcome!',
+			'text' => 'Welcome to the Online Application',
 			'description' => 'Greetings on HomeScreen' 
+		)
+	),
+	array
+	(
+		'phrase' => 'HomeNoAccountOrAccessCode',
+		'phrasentext' => array
+		(
+			'sprache' => 'de-AT',
+			'text' => 'Sie haben noch keinen Account oder Zugangscode?',
+			'description' => 'Registrierungstext am HomeScreen' 
+		)
+	),
+	array
+	(
+		'phrase' => 'HomeNoAccountOrAccessCode',
+		'phrasentext' => array
+		(
+			'sprache' => 'en-US',
+			'text' => 'You don`t have an account or access code?',
+			'description' => 'Registration text on HomeScreen' 
+		)
+	),
+	array
+	(
+		'phrase' => 'HomeAccessCodeAvailable',
+		'phrasentext' => array
+		(
+			'sprache' => 'de-AT',
+			'text' => 'Sie haben bereits einen Zugangscode?',
+			'description' => 'Mit Access Code einloggen am HomeScreen' 
+		)
+	),
+	array
+	(
+		'phrase' => 'HomeAccessCodeAvailable',
+		'phrasentext' => array
+		(
+			'sprache' => 'en-US',
+			'text' => 'You already have an access code?',
+			'description' => 'Login with access code on HomeScreen' 
+		)
+	),
+	array
+	(
+		'phrase' => 'HomeLogin',
+		'phrasentext' => array
+		(
+			'sprache' => 'de-AT',
+			'text' => 'Dann loggen Sie sich hier ein',
+			'description' => 'Login am HomeScreen' 
+		)
+	),
+	array
+	(
+		'phrase' => 'HomeLogin',
+		'phrasentext' => array
+		(
+			'sprache' => 'en-US',
+			'text' => 'Login here',
+			'description' => 'Login on HomeScreen' 
 		)
 	)
 );
