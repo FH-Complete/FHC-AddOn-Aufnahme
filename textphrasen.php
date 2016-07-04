bda9b561e1e3c48d06d485dcaad8d6e66a5444e7
Nikolaus Krondraf <krondraf@technikum-wien.at>
2016-07-04 11:34:49 +0200

    Phrasen hinzugefügt

--- textphrasen.php
+++ textphrasen.php
@@ -6,7 +6,7 @@ $textphrasen = array
 		'phrase' => 'HomeGreetings',
 		'phrasentext' => array
 		(
-			'sprache' => 'de-AT',
+			'sprache' => 'German',
 			'text' => 'Willkommen bei der Online Bewerbung',
 			'description' => 'Begrüßungstext am HomeScreen' 
 		)
@@ -16,7 +16,7 @@ $textphrasen = array
 		'phrase' => 'HomeGreetings',
 		'phrasentext' => array
 		(
-			'sprache' => 'en-US',
+			'sprache' => 'English',
 			'text' => 'Welcome to the Online Application',
 			'description' => 'Greetings on HomeScreen' 
 		)
@@ -26,7 +26,7 @@ $textphrasen = array
 		'phrase' => 'HomeNoAccountOrAccessCode',
 		'phrasentext' => array
 		(
-			'sprache' => 'de-AT',
+			'sprache' => 'German',
 			'text' => 'Sie haben noch keinen Account oder Zugangscode?',
 			'description' => 'Registrierungstext am HomeScreen' 
 		)
@@ -36,7 +36,7 @@ $textphrasen = array
 		'phrase' => 'HomeNoAccountOrAccessCode',
 		'phrasentext' => array
 		(
-			'sprache' => 'en-US',
+			'sprache' => 'English',
 			'text' => 'You don`t have an account or access code?',
 			'description' => 'Registration text on HomeScreen' 
 		)
@@ -46,7 +46,7 @@ $textphrasen = array
 		'phrase' => 'HomeAccessCodeAvailable',
 		'phrasentext' => array
 		(
-			'sprache' => 'de-AT',
+			'sprache' => 'German',
 			'text' => 'Sie haben bereits einen Zugangscode?',
 			'description' => 'Mit Access Code einloggen am HomeScreen' 
 		)
@@ -56,7 +56,7 @@ $textphrasen = array
 		'phrase' => 'HomeAccessCodeAvailable',
 		'phrasentext' => array
 		(
-			'sprache' => 'en-US',
+			'sprache' => 'English',
 			'text' => 'You already have an access code?',
 			'description' => 'Login with access code on HomeScreen' 
 		)
@@ -66,7 +66,7 @@ $textphrasen = array
 		'phrase' => 'HomeLogin',
 		'phrasentext' => array
 		(
-			'sprache' => 'de-AT',
+			'sprache' => 'German',
 			'text' => 'Dann loggen Sie sich hier ein',
 			'description' => 'Login am HomeScreen' 
 		)
@@ -76,9 +76,129 @@ $textphrasen = array
 		'phrase' => 'HomeLogin',
 		'phrasentext' => array
 		(
-			'sprache' => 'en-US',
-			'text' => 'Login here',
+			'sprache' => 'English',
+			'text' => '',
 			'description' => 'Login on HomeScreen' 
+		)
+	),
+    array
+	(
+		'phrase' => 'ChooseDateForTest',
+		'phrasentext' => array
+		(
+			'sprache' => 'German',
+			'text' => 'Ihre Bewerbung ist eingelangt, bitte wählen Sie einen Termin für das erste Aufnahmeverfahren aus:',
+			'description' => 'Terminauswahl für Aufnahmeverfahren' 
+		)
+	),
+	array
+	(
+		'phrase' => 'ChooseDateForTest',
+		'phrasentext' => array
+		(
+			'sprache' => 'English',
+			'text' => '',
+			'description' => 'Date selection for test'
+		)
+	),
+    array
+	(
+		'phrase' => 'PersonalInformation',
+		'phrasentext' => array
+		(
+			'sprache' => 'German',
+			'text' => 'Angaben zur Person',
+			'description' => 'Persönliche Daten'
+		)
+	),
+	array
+	(
+		'phrase' => 'PersonalInformation',
+		'phrasentext' => array
+		(
+			'sprache' => 'English',
+			'text' => '',
+			'description' => 'Personal information'
+		)
+	),
+    array
+	(
+		'phrase' => 'DifferentAddress',
+		'phrasentext' => array
+		(
+			'sprache' => 'German',
+			'text' => 'Meine Zustelladresse weicht von der Heimatadresse ab.',
+			'description' => 'Alternative Zustelladresse' 
+		)
+	),
+	array
+	(
+		'phrase' => 'DifferentAddress',
+		'phrasentext' => array
+		(
+			'sprache' => 'English',
+			'text' => '',
+			'description' => 'Alternative address for service'
+		)
+	),
+    array
+	(
+		'phrase' => 'PleaseUploadDocuments',
+		'phrasentext' => array
+		(
+			'sprache' => 'German',
+			'text' => 'Bitte laden Sie Ihre Geburtsurkunde und Ihren Staatbürgerschaftsnachweis im Format .pdf, .doc, oder .jpg hoch. Die maximale Dateigröße beträgt 2MB.',
+			'description' => 'Beschreibung für Dokumentenupload' 
+		)
+	),
+	array
+	(
+		'phrase' => 'PleaseUploadDocuments',
+		'phrasentext' => array
+		(
+			'sprache' => 'English',
+			'text' => '',
+			'description' => 'Description for document upload'
+		)
+	),
+    array
+	(
+		'phrase' => 'AdmissionRequirements',
+		'phrasentext' => array
+		(
+			'sprache' => 'German',
+			'text' => 'Die aktuellen Zugangsvoraussetzungen finden Sie auf der Website der Fachhochschule St. Pölten. Um sich über die Zugangsvoraussetzungen zu informieren, klicken sie bitte hier.',
+			'description' => 'Beschreibung zur den Zugangsvoraussetzungen' 
+		)
+	),
+	array
+	(
+		'phrase' => 'AdmissionRequirements',
+		'phrasentext' => array
+		(
+			'sprache' => 'English',
+			'text' => '',
+			'description' => 'Description for the admission requirements'
+		)
+	),
+    array
+	(
+		'phrase' => 'UploadDiploma',
+		'phrasentext' => array
+		(
+			'sprache' => 'German',
+			'text' => 'Bitte laden Sie Ihr Abschlusszeugnis im Format .pdf, .doc oder .jpg hoch:',
+			'description' => 'Beschreibung für Upload ' 
+		)
+	),
+	array
+	(
+		'phrase' => 'UploadDiploma',
+		'phrasentext' => array
+		(
+			'sprache' => 'English',
+			'text' => '',
+			'description' => ''
 		)
 	)
 );
