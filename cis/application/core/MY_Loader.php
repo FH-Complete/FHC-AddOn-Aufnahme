fb872d9dc99f2a8b14c11252ce8ea12f2e24ed2f
Paminger <christian@paminger.at>
2016-07-06 20:16:06 +0200

    Kosmetik

--- cis/application/core/MY_Loader.php
+++ cis/application/core/MY_Loader.php
@@ -17,39 +17,35 @@ class MY_Loader extends CI_Loader
 	}
     }
     
-    function getPhrase($phrase, $sprache, $oe_kurzbz="", $orgform_kurzbz="")
+    function getPhrase($phrase, $sprache, $oe_kurzbz = '', $orgform_kurzbz = '')
     {
-	if(isset($this->session->userdata()["phrasen"]))
-	{
-	    $phrasen = $this->session->userdata()["phrasen"];
-	    foreach($phrasen as $p)
-	    {
-		if(($p->phrase == $phrase) && ($p->orgeinheit_kurzbz == $oe_kurzbz) && ($p->orgform_kurzbz == $orgform_kurzbz))
+		if(isset($this->session->userdata()["phrasen"]))
 		{
-		    if ($this->config->item('display_phrase_name'))
-		    {
-			return $p->phrase;
-		    }
-		    else
-		    {
-			return $p->text;
-		    }
-		}
-	    }
-	    
-	    foreach($phrasen as $p)
-	    {
-		if(($p->phrase == $phrase) && ($p->orgeinheit_kurzbz == $oe_kurzbz))
-		{
-		    if ($this->config->item('display_phrase_name'))
-		    {
-			return $p->phrase;
-		    }
-		    else
-		    {
-			return $p->text;
-		    }
-		}
+			$phrasen = $this->session->userdata()["phrasen"];
+			foreach($phrasen as $p)
+			{
+				if(($p->phrase == $phrase) && ($p->orgeinheit_kurzbz == $oe_kurzbz) && ($p->orgform_kurzbz == $orgform_kurzbz))
+				{
+					if ($this->config->item('display_phrase_name'))
+						return $p->phrase;
+					else
+						return $p->text;
+				}
+			}
+			
+			foreach($phrasen as $p)
+			{
+			if(($p->phrase == $phrase) && ($p->orgeinheit_kurzbz == $oe_kurzbz))
+			{
+				if ($this->config->item('display_phrase_name'))
+				{
+				return $p->phrase;
+				}
+				else
+				{
+				return $p->text;
+				}
+			}
 	    }
 	    
 	    foreach($phrasen as $p)
