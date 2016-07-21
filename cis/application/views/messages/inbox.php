81529a294318ea2b30c111a5113112ed06f71ce0
Stefan Puraner <puraner@technikum-wien.at>
2016-07-21 10:26:06 +0200

    refactored message view

--- cis/application/views/messages/inbox.php
+++ cis/application/views/messages/inbox.php
@@ -5,34 +5,34 @@
 	    <?php echo $this->lang->line('msg_newMessage'); ?>
 	</button>
     </a>
-    <!-- write JS function with API call to set message to status read -->
+    <!-- TODO write JS function with API call to set message to status read -->
     <?php if($view=="messages") { ?>
 	<div id="messages">
 	    <?php foreach($messages as $msg) { ?>
-	    <div id="message_<?php echo $msg->message_id; ?>">
-		<div style="float: left; width: 5%;">
-		    <span class='<?php echo ($msg->status == MSG_STATUS_READ) ? "icon-read" : ($msg->status == MSG_STATUS_UNREAD) ? "icon-unread" : "" ;?>'></span>
+	    <div id="message_<?php echo $msg->message_id; ?>" class="row">
+		<div class="col-sm-1">
+		    <span class='<?php echo ($msg->status == MSG_STATUS_READ) ? "icon-read" : ($msg->status == MSG_STATUS_UNREAD) ? "icon-unread" : "" ;?>'>&nbsp;</span>
 		</div>
-		<div style="float: left; width: 50%;">
+		<div class="col-sm-5">
 		    <a class="collapsed" data-toggle='collapse' data-target='#message_body_<?php echo $msg->message_id; ?>'>
 			<?php echo $msg->subject; ?>
 		    </a>
 		</div>
-		<div style="float: left; width: 15%;">
+		<div class="col-sm-2">
 		    <?php
 			$time = strtotime($msg->insertamum);
 			echo date("d.m.Y h:m", $time); 
 		    ?>
 		</div>
-		<div style="float: left; width: 10%;"><span class="icon-paperclip"></span></div>
-		<div style="float: left; width: 10%;">
+		<div class="col-sm-1"><span class="icon-paperclip"></span></div>
+		<div class="col-sm-2">
 		    <a href="<?php echo base_url($this->config->config['index_page'].'/Messages/answerMessage/'.$msg->message_id.'/'.$msg->oe_kurzbz); ?>">
 			<button class="answer btn btn-sm btn-primary" id="answer_<?php echo $msg->message_id; ?>">
 			    <?php echo $this->lang->line('msg_answer'); ?>
 			</button>
 		    </a>
 		</div>
-		<div style="float: left; width: 10%;">
+		<div class="col-sm-1">
 		    <a href="<?php echo base_url($this->config->config['index_page'].'/Messages/deleteMessage/'.$msg->message_id); ?>">
 			<button class="btn btn-sm btn-primary icon-trash" id="answer_<?php echo $msg->message_id; ?>"></button>
 		    </a>
