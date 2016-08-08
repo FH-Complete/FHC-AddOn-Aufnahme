72ece671764fed5c11beb5c14639d005efe79672
Stefan Puraner <puraner@technikum-wien.at>
2016-08-08 10:06:54 +0200

    refactored footer

--- cis/application/views/templates/footer.php
+++ cis/application/views/templates/footer.php
@@ -1,25 +1,33 @@
-<div class="container">
-    <div class="pull-right">
-	<h1><?php echo $this->config->item("organisation")["bezeichnung"]; ?></h1>
-	<span><?php echo $this->config->item("organisation")["strasse"]; ?></span></br>
-	<span><?php echo $this->config->item("organisation")["plz"]; ?>&nbsp;<?php echo $this->config->item("organisation")["ort"]; ?></span></br>
-	
-	<span><?php echo ($this->config->item("organisation")["telefon"] != "") ?  "T: ".$this->config->item("organisation")["telefon"] : ""; ?></span></br>
-	<span><?php echo ($this->config->item("organisation")["fax"] != "") ?  "F: ".$this->config->item("organisation")["fax"] : ""; ?></span></br>
-	<span><?php echo ($this->config->item("organisation")["mail"] != "") ?  "E-Mail: ".$this->config->item("organisation")["mail"] : ""; ?></span></br>
+<footer id="portal-footer" class="module shadow-top" role="contentinfo">
+    <div class="container">
+	<section class="row">
+	    <div class="col-sm-4">
+	    </div>
+	    <div class="col-sm-4">
+	    </div>
+	    <div class="col-sm-4 adress">
+		<h3><?php echo $this->config->item("organisation")["bezeichnung"]; ?></h3>
+		<span><?php echo $this->config->item("organisation")["strasse"]; ?></span></br>
+		<span><?php echo $this->config->item("organisation")["plz"]; ?>&nbsp;<?php echo $this->config->item("organisation")["ort"]; ?></span></br>
+
+		<span><?php echo ($this->config->item("organisation")["telefon"] != "") ?  "T: ".$this->config->item("organisation")["telefon"] : ""; ?></span></br>
+		<span><?php echo ($this->config->item("organisation")["fax"] != "") ?  "F: ".$this->config->item("organisation")["fax"] : ""; ?></span></br>
+		<span><?php echo ($this->config->item("organisation")["mail"] != "") ?  "E-Mail: ".$this->config->item("organisation")["mail"] : ""; ?></span></br>
+	    </div>
+	</section>
     </div>
-</div>
-<div id="meta-footer" class="bar">
+</footer>
+<footer id="meta-footer" class="bar">
     <div class="container-fluid">
 	<nav role="navigation">
 	    <div>
 		<ul class="nav navbar-nav navbar-right">
-		    <li><?php echo $this->config->item("impressumLink"); ?></li>
-		    <li><?php echo $this->config->item("anfahrtLink"); ?></li>
+		    <li class="impressum"><?php echo $this->config->item("impressumLink"); ?></li>
+		    <li class="anfahrt"><?php echo $this->config->item("anfahrtLink"); ?></li>
 		</ul>
 	    </div>
 	</nav>
     </div>
-</div>
+</footer>
 </body>
 </html>
