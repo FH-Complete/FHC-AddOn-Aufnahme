c81d45cd830155fae597176149de043f17d3e67c
Stefan Puraner <puraner@technikum-wien.at>
2016-09-30 09:58:30 +0200

    fixed issu #646

--- cis/application/views/bewerbung/studiengang.php
+++ cis/application/views/bewerbung/studiengang.php
@@ -8,7 +8,7 @@
 ?>
 <a class="<?php echo (isset($studiengang_kz) && ($studiengang_kz == $studiengang->studiengang_kz)) ? "" : "collapsed"?>" data-toggle='collapse' data-target='#<?php echo $studiengang->studiengang_kz; ?>'>
 	<h1 class="studiengang-list-item"><?php echo $studiengang->bezeichnung ?>
-		(<?php echo $studiengang->orgform_kurzbz; ?>)
+		(<?php echo $studiengang->studienplan->orgform_kurzbz; ?>)
 	</h1>
 </a>
 <button class="btn btn-sm btn-primary btn-background" type="button" onclick="confirmStorno(<?php echo $studiengang->studiengang_kz; ?>)">
