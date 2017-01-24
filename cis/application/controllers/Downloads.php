37a32272864a0f4a85c7fb3423fba0cf31962456
bison-paolo <bison.paolo@gmail.com>
2017-01-24 11:09:23 +0100

    Downloads controller and model

--- cis/application/controllers/Downloads.php
+++ cis/application/controllers/Downloads.php
@@ -5,127 +5,78 @@
  *
  * @package default
  */
-class Downloads extends MY_Controller
+class Downloads extends UI_Controller
 {
-    private $_person_id;
-    private $_studiensemester_kurzbz;
-
     /**
      *
      */
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('prestudent_model', "PrestudentModel");
-        $this->load->model('studiengang_model', "StudiengangModel");
-        $this->load->model('person_model', 'PersonModel');
-        $this->_data["sprache"] = $this->get_language();
-        $this->_data["numberOfUnreadMessages"] = $this->_getNumberOfUnreadMessages();
+        
+        $this->load->model('crm/Prestudent_model', 'PrestudentModel');
+		$this->load->model('organisation/Studiengang_model', 'StudiengangModel');
+		$this->load->model('organisation/Studiensemester_model', 'StudiensemesterModel');
+		$this->load->model('person/Person_model', 'PersonModel');
+		$this->load->model('system/Message_model', 'MessageModel');
+        
+        // 
+		$this->setData('sprache', $this->getCurrentLanguage());
+		
+        $this->setData('numberOfUnreadMessages', $this->MessageModel->getCountUnreadMessages());
     }
-
-
+	
     /**
      *
      */
     public function index()
     {
-        $this->checkLogin();
-
-        $person = null;
-        if (isset($this->session->{'Person.getPerson'}))
-        {
-            $person = $this->session->{'Person.getPerson'};
-            if (hasData($person))
-            {
-                if (isset($person->retval->person_id) && is_numeric($person->retval->person_id))
-                {
-                    $this->_person_id = $person->retval->person_id;
-                }
-            }
-        }
-
         $this->_loadData();
-
-        $this->_loadLanguage($this->_data["sprache"]);
-
-        $this->load->view('downloads', $this->_data);
+		
+        $this->load->view('downloads', $this->getAllData());
     }
-
-
+	
     /**
      *
      */
     private function _loadData()
     {
         //load person data
-        $this->_data["person"] = $this->_loadPerson();
-
-        $this->_data["prestudent"] = $this->_loadPrestudent($this->_person_id);
-
-        $this->_data["studiengaenge"] = array();
-        foreach ($this->_data["prestudent"] as $prestudent)
-        {
-            if ($prestudent->studiengang_kz != null)
-            {
-                $studiengang = $this->_loadStudiengnag($prestudent->studiengang_kz);
-                $this->_data["studiengaenge"][$studiengang->oe_kurzbz] = $studiengang->bezeichnung;
-            }
-        }
+		$this->setData('person', $this->PersonModel->getPerson());
+		
+		$studiengaengeArray = array();
+		
+		$studiensemester = $this->StudiensemesterModel->getNextStudiensemester('WS');
+		if (hasData($studiensemester))
+		{
+			$this->setData('studiensemester', $studiensemester);
+			$studiengaenge = $this->StudiengangModel->getAppliedStudiengang(
+				$this->getData('studiensemester')->studiensemester_kurzbz,
+				'',
+				'Interessent',
+                true
+			);
+			
+			if (hasData($studiengaenge))
+			{
+				foreach ($studiengaenge->retval as $studiengang)
+				{
+					$studiengaengeArray[$studiengang->oe_kurzbz] = $studiengang->bezeichnung;
+				}
+			}
+		}
+		
+		$this->setRawData('studiengaenge', $studiengaengeArray);
+		
+		$this->setData(
+		    'prestudent',
+            $this->PrestudentModel->getLastStatuses(
+                $this->getData('person')->person_id,
+                $this->getData('studiensemester')->studiensemester_kurzbz,
+                null,
+                null,
+                true
+            )
+		);
     }
-
-
-    /**
-     *
-     * @param unknown $person_id
-     * @return unknown
-     */
-    private function _loadPrestudent($person_id)
-    {
-        $this->PrestudentModel->getPrestudent(array("person_id" => $person_id));
-        if ($this->PrestudentModel->isResultValid() === true)
-        {
-            return $this->PrestudentModel->result->retval;
-        }
-        else
-        {
-            $this->_setError(true, $this->PrestudentModel->getErrorMessage());
-        }
-    }
-
-
-    private function _loadStudiengnag($studiengang_kz)
-    {
-        $this->StudiengangModel->getStudiengang($studiengang_kz);
-        if ($this->StudiengangModel->isResultValid() === true)
-        {
-            return $this->StudiengangModel->result->retval[0];
-        }
-        else
-        {
-            $this->_setError(true, $this->StudiengangModel->getErrorMessage());
-        }
-    }
-
-
-    private function _loadPerson()
-    {
-        $this->PersonModel->getPersonen(array("person_id" => $this->_person_id));
-        if ($this->PersonModel->isResultValid() === true)
-        {
-            if (count($this->PersonModel->result->retval) == 1)
-            {
-                return $this->PersonModel->result->retval[0];
-            }
-            else
-            {
-                return $this->PersonModel->result->retval;
-            }
-        }
-        else
-        {
-            $this->_setError(true, $this->PersonModel->getErrorMessage());
-        }
-    }
-
-
 }
